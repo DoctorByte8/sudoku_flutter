8fb2c12595e589e3d742c21ae2077546adc99b9a
Thiago Aragão <thiago.alpha.06@gmail.com>
2025-01-09 16:12:23 -0300

    Início app2
    Início do desenvolvimento do app2. Import do sqlite e declaração dele no pubspec. Criação do arquivo do historico para tela de historico.

---
+++ lib/historico.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sqflite/sqflite.dart';
+
+class HistoricoApp extends StatefulWidget {
+  @override
+  HistoricoAppState createState() => HistoricoAppState();
+}
+
+class HistoricoAppState extends State<HistoricoApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(0);
+  }
+}
